20e718532915b8c846edf71aa966173636c0bac1
Shafiq9845 <siddiqaboobakkarsiddiq@gmail.com>
2024-02-27 18:44:14 +0530

    navbar added

---
+++ my_world/src/app/navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "../asset/navbar.css"
+import Link from 'next/link'
+const navbar = () => {
+  return (
+
+    <nav className="main_container">
+
+        <div className="left_container">
+            <h3>My World</h3>
+        </div>
+
+        <div className="center_container">
+
+            <Link href="/">Home</Link>
+            <Link href="/About-us">About Us</Link>
+            <Link href="/our-work">Our Work</Link>
+            <Link href="/contact-us">Contact Us</Link>
+
+        </div>
+
+        <div className="right_container">
+        <a href="" class="fa fa-linkedin"></a>
+        <a href="https://twitter.com/ShafiqSiddiq11" class="fa fa-twitter"></a>
+        <a href="https://www.instagram.com/iem_shafiq" class="fa fa-instagram"></a>
+        <a href="https://github.com/Shafiq9845" class="fa fa-github"></a>
+        </div>
+
+    </nav>
+
+  )
+}
+
+export default navbar
