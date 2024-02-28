f16d95bc212fc20ce50db6900cd5248dc77ac7ff
Shafiq9845 <siddiqaboobakkarsiddiq@gmail.com>
2024-02-28 19:44:43 +0530

    image added

--- my_world/src/app/body/page.js
+++ my_world/src/app/body/page.js
@@ -1,14 +1,24 @@
 import Image from "next/image"
-// import Img from "../images/profile.jpg"
+import Img from "../images/profile.jpg"
 import "../../asset/body.css"
+import Script from "next/script"
 import React from 'react'
-
+import js from"../javascript/text"
 const page = () => {
   return (
     <>
-    <div className="image-container">
-        {/* <Image src={Img} width={500} height={410} alt="image"></Image> */}
+    <div className="main-container">
+      <div className="image-container">
+        <Image src={Img} alt="image" loading="lazy" className="image"></Image>
+
+      </div>
+        <h1>
+          <a href="" class="typewrite" data-period="2000" data-type='[ "Hi, Im Si.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
+            <span class="wrap"></span>
+          </a>
+        </h1>
     </div>
+    <Script src={js}></Script>
     </>
   )
 }
